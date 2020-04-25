4d06adc99734e71ac1888e23f95aaae10d189929
banaanihillo <banaanihillo@protonmail.ch>
2020-04-25 18:12:45 +0300

    Commited exercise 5-7 (Blog frontend)
    Modified blog display to show only the title by default,
    and to expand by clicking

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from "react"
-const Blog = ({blog}) => (
-    <div>
-        {blog.title}, by {blog.author}: {blog.url}
-    </div>
-)
-
+import React, {useState} from "react"
+const Blog = (props) => {
+    const {blog} = props
+    const [displayAll, setDisplay] = useState(false)
+    
+    const toggleDisplay = () => {
+        setDisplay(!displayAll)
+    }
+    return (
+        <div>
+            {!displayAll
+                ? <div> {blog.title}
+                    <button onClick = {toggleDisplay}> Expand </button>
+                </div>
+                : <div>
+                    <p> {blog.title}
+                        <button onClick = {toggleDisplay}> Collapse </button>
+                    </p>
+                    <p> {blog.author} </p>
+                    <p> {blog.url} </p>
+                    <p> Times thanked: {blog.thanks}
+                        <button onClick = {() => console.log("Click")}>
+                            You can click on this if you really want to
+                        </button>
+                    </p>
+            </div>
+            }
+        </div>
+    )
+    
+}
 export default Blog
