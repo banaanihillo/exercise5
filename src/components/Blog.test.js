83efb57a26240ab95fd3446895f3f0a3daed8c94
banaanihillo <banaanihillo@protonmail.ch>
2020-04-27 16:03:03 +0300

    Commited exercise5-14 (Blog frontend tests)
    Added test for displaying additional blog information,
    also fixed(?) the test for exercise5-13

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,20 +1,51 @@
 import React from "react"
 import "@testing-library/jest-dom/extend-expect"
-import {render} from "@testing-library/react"
+import {render, fireEvent} from "@testing-library/react"
 import Blog from "./Blog"
+import {prettyDOM} from "@testing-library/dom"
 
 test("Rendering the blog works", () => {
     const blog = {
         title: "Something something something",
-        author: "An Author"
+        author: "An Author",
+        url: "an address",
+        thanks: 2
     }
     const component = render(
         <Blog blog = {blog} />
     )
-    console.log(blog)
+    component.debug()
     expect(component.container).toHaveTextContent(
-        "Something something something",
-        "An Author"
+        "Something something something"
     )
-    expect(blog.thanks).not.toBeDefined
+    expect(component.container).not.toHaveTextContent("an address")
+})
+
+test("Additional blog information is rendered upon button click", () => {
+    const blog = {
+        title: "Buttons and Displays",
+        author: "Displayist",
+        url: "/info",
+        thanks: 5,
+        user: "User of this test"
+    }
+    const user = {
+        name: "User of this test"
+    }
+    const toggleDisplay = jest.fn()
+    const component = render(
+        <Blog
+            blog = {blog}
+            toggleDisplay = {toggleDisplay}
+            user = {user} />
+    )
+    component.debug()
+    expect(component.container).not.toHaveTextContent("Times thanked")
+
+    const button = component.getByText("Expand")
+    console.log(prettyDOM(button))
+    fireEvent.click(button)
+    component.debug()
+    expect(component.container).toHaveTextContent("Times thanked")
+    expect(component.container).toHaveTextContent("/info")
 })
